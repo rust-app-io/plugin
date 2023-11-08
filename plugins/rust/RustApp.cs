e72796449254fb10cb2d6d8c014553ebbff55a77
Хуган <hougan.main@gmail.com>
2023-11-08 16:25:13 +0200

    Поддержка отправки контактов

--- plugins/rust/RustApp.cs
+++ plugins/rust/RustApp.cs
@@ -30,6 +30,7 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 using System.IO;
+using ConVar;
 
 
 namespace Oxide.Plugins
@@ -37,180 +38,6 @@ namespace Oxide.Plugins
   [Info("RustApp.IO", "Hougan & Xacku & Olkuts", "1.0.0")]
   public class RustApp : RustPlugin
   {
-    private struct Array2D<T>
-    {
-      public T[] _items;
-
-      private int _width;
-
-      private int _height;
-
-
-      public int getBy(int x, int y)
-      {
-        int num = Mathf.Clamp(x, 0, _width - 1);
-        int num2 = Mathf.Clamp(y, 0, _height - 1);
-        return num2 * _width + num;
-      }
-
-      public Array2D(T[] items, int width, int height)
-      {
-        _items = items;
-        _width = width;
-        _height = height;
-      }
-    }
-
-    public static class MapImageRendererV2
-    {
-
-      private static readonly Vector3 StartColor = new Vector3(0.28627452f, 23f / 85f, 0.24705884f);
-
-      private static readonly Vector4 WaterColor = new Vector4(0.0470588235294118f, 0.0470588235294118f, 0.0470588235294118f, 0f);
-
-      private static readonly Vector4 GravelColor = new Vector4(0.25f, 37f / 152f, 0.22039475f, 1f);
-
-      private static readonly Vector4 DirtColor = new Vector4(0.6f, 0.47959462f, 0.33f, 1f);
-
-      private static readonly Vector4 SandColor = new Vector4(0.7f, 0.65968585f, 0.5277487f, 1f);
-
-      private static readonly Vector4 GrassColor = new Vector4(0.35486364f, 0.37f, 0.2035f, 1f);
-
-      private static readonly Vector4 ForestColor = new Vector4(0.24843751f, 0.3f, 9f / 128f, 1f);
-
-      private static readonly Vector4 RockColor = new Vector4(0.4f, 0.39379844f, 0.37519377f, 1f);
-
-      private static readonly Vector4 SnowColor = new Vector4(0.86274517f, 0.9294118f, 0.94117653f, 1f);
-
-      private static readonly Vector4 PebbleColor = new Vector4(7f / 51f, 0.2784314f, 0.2761563f, 1f);
-
-      private static readonly Vector4 OffShoreColor = new Vector4(0.0370588235294118f, 0.0370588235294118f, 0.0370588235294118f, 0f);
-
-      private static readonly Vector3 SunDirection = Vector3.Normalize(new Vector3(0.95f, 2.87f, 2.37f));
-
-      private const float SunPower = 0.65f;
-
-      private const float Brightness = 1.05f;
-
-      private const float Contrast = 0.94f;
-
-      private const float OceanWaterLevel = 0f;
-
-      private static readonly Vector3 Half = new Vector3(0.5f, 0.5f, 0.5f);
-
-      public static byte[] Render(out int imageWidth, out int imageHeight, out Color background, float scale = 0.5f, bool lossy = true)
-      {
-        imageWidth = 0;
-        imageHeight = 0;
-        background = OffShoreColor;
-        TerrainTexturing instance = TerrainTexturing.Instance;
-        if (instance == null)
-        {
-          return null;
-        }
-        Terrain component = instance.GetComponent<Terrain>();
-        TerrainMeta component2 = instance.GetComponent<TerrainMeta>();
-        TerrainHeightMap terrainHeightMap = instance.GetComponent<TerrainHeightMap>();
-        TerrainSplatMap terrainSplatMap = instance.GetComponent<TerrainSplatMap>();
-        if (component == null || component2 == null || terrainHeightMap == null || terrainSplatMap == null)
-        {
-          return null;
-        }
-        int mapRes = (int)((float)World.Size * Mathf.Clamp(scale, 0.1f, 4f));
-        float invMapRes = 1f / (float)mapRes;
-        if (mapRes <= 0)
-        {
-          return null;
-        }
-        imageWidth = mapRes + 1000;
-        imageHeight = mapRes + 1000;
-        Color[] array = new Color[imageWidth * imageHeight];
-        Array2D<Color> output = new Array2D<Color>(array, imageWidth, imageHeight);
-        System.Threading.Tasks.Parallel.For(0, imageHeight, delegate (int y)
-        {
-          y -= 500;
-          float y2 = (float)y * invMapRes;
-          int num = mapRes + 500;
-          for (int i = -500; i < num; i++)
-          {
-            float x2 = (float)i * invMapRes;
-            Vector3 startColor = StartColor;
-            float height = GetHeight(terrainHeightMap, x2, y2);
-            float num2 = Math.Max(Vector3.Dot(GetNormal(terrainHeightMap, x2, y2), SunDirection), 0f);
-            startColor = Vector3.Lerp(startColor, GravelColor, GetSplat(terrainSplatMap, x2, y2, 128) * GravelColor.w);
-            startColor = Vector3.Lerp(startColor, PebbleColor, GetSplat(terrainSplatMap, x2, y2, 64) * PebbleColor.w);
-            startColor = Vector3.Lerp(startColor, RockColor, GetSplat(terrainSplatMap, x2, y2, 8) * RockColor.w);
-            startColor = Vector3.Lerp(startColor, DirtColor, GetSplat(terrainSplatMap, x2, y2, 1) * DirtColor.w);
-            startColor = Vector3.Lerp(startColor, GrassColor, GetSplat(terrainSplatMap, x2, y2, 16) * GrassColor.w);
-            startColor = Vector3.Lerp(startColor, ForestColor, GetSplat(terrainSplatMap, x2, y2, 32) * ForestColor.w);
-            startColor = Vector3.Lerp(startColor, SandColor, GetSplat(terrainSplatMap, x2, y2, 4) * SandColor.w);
-            startColor = Vector3.Lerp(startColor, SnowColor, GetSplat(terrainSplatMap, x2, y2, 2) * SnowColor.w);
-            float num3 = 0f - height;
-            if (num3 > 0f)
-            {
-              startColor = Vector3.Lerp(startColor, WaterColor, Mathf.Clamp(0.5f + num3 / 5f, 0f, 1f));
-              startColor = Vector3.Lerp(startColor, OffShoreColor, Mathf.Clamp(num3 / 50f, 0f, 1f));
-              num2 = 0.5f;
-            }
-            startColor += (num2 - 0.5f) * 0.65f * startColor;
-            startColor = (startColor - Half) * 0.94f + Half;
-            startColor *= 1.05f;
-
-            output._items[output.getBy(i + 500, y + 500)] = new Color(startColor.x, startColor.y, startColor.z);
-          }
-        });
-
-
-        background = output._items[output.getBy(0, 0)];
-        return EncodeToFile(imageWidth, imageHeight, array, lossy);
-      }
-
-
-      static float GetHeight(TerrainHeightMap terrainHeightMap, float x, float y)
-      {
-        return terrainHeightMap.GetHeight(x, y);
-      }
-      static Vector3 GetNormal(TerrainHeightMap terrainHeightMap, float x, float y)
-      {
-        return terrainHeightMap.GetNormal(x, y);
-      }
-      static float GetSplat(TerrainSplatMap terrainSplatMap, float x, float y, int mask)
-      {
-        return terrainSplatMap.GetSplat(x, y, mask);
-      }
-
-      private static byte[] EncodeToFile(int width, int height, Color[] pixels, bool lossy)
-      {
-        Texture2D texture2D = null;
-        try
-        {
-          texture2D = new Texture2D(width, height);
-          texture2D.SetPixels(pixels);
-          texture2D.Apply();
-          return lossy ? texture2D.EncodeToJPG(85) : texture2D.EncodeToPNG();
-        }
-        finally
-        {
-          if (texture2D != null)
-          {
-            UnityEngine.Object.Destroy(texture2D);
-          }
-        }
-      }
-
-      private static Vector3 UnpackNormal(Vector4 value)
-      {
-        value.x *= value.w;
-        Vector3 result = default(Vector3);
-        result.x = value.x * 2f - 1f;
-        result.y = value.y * 2f - 1f;
-        Vector2 vector = new Vector2(result.x, result.y);
-        result.z = Mathf.Sqrt(1f - Mathf.Clamp(Vector2.Dot(vector, vector), 0f, 1f));
-        return result;
-      }
-    }
-
-
     #region Classes
 
     // From RustStats by Sanlerus  
@@ -433,6 +260,16 @@ namespace Oxide.Plugins
         request.Execute();
       }
 
+      public static void SendContact(string secret, PluginContactPayload payload, Action<bool> callback)
+      {
+        var request = new StableRequest<object>($"{COURT_URL}/contact", RequestMethod.POST, payload, secret, "Sending contact");
+
+        request.onException += (a) => callback(false);
+        request.onComplete += (a, b) => callback(true);
+
+        request.Execute();
+      }
+
       public static void SendChatMessages(string secret, PluginChatMessagesPayload payload, Action<PluginChatMessagesPayload> fallback)
       {
         var request = new StableRequest<object>($"{COURT_URL}/chat", RequestMethod.POST, payload, secret, "Updating state");
@@ -523,6 +360,18 @@ namespace Oxide.Plugins
       public PluginStatePayload(List<PluginPlayerPayload> players)
       {
         this.players = players;
+      }
+    }
+
+    class PluginContactPayload
+    {
+      public string steam_id;
+      public string message;
+
+      public PluginContactPayload(string steam_id, string message)
+      {
+        this.steam_id = steam_id;
+        this.message = message;
       }
     }
 
@@ -713,6 +562,16 @@ namespace Oxide.Plugins
         messages = new List<PluginChatMessageEntry>();
       }
 
+      public void SendContact(string steam_id, string message, Action<bool> callback)
+      {
+        if (!CheckCondition())
+        {
+          return;
+        }
+
+        AvailableRequests.SendContact(secret, new PluginContactPayload(steam_id, message), callback);
+      }
+
       public void SendReports()
       {
         if (!CheckCondition() || reports.Count == 0)
@@ -813,7 +672,6 @@ namespace Oxide.Plugins
       }
 
 
-
       private class QueueKickPayload
       {
         public string steam_id;
@@ -885,9 +743,8 @@ namespace Oxide.Plugins
         }
 
         Notices[payload.steam_id] = payload.value;
-        NoticeStateSet(payload.steam_id, payload.value);
 
-        return true;
+        return NoticeStateSet(payload.steam_id, payload.value);
       }
 
       private void NoticeClear()
@@ -895,10 +752,13 @@ namespace Oxide.Plugins
         Notices.Keys.ToList().ForEach(v => NoticeStateSet(v, false));
       }
 
-      private void NoticeStateSet(string steam_id, bool value)
+      private bool NoticeStateSet(string steam_id, bool value)
       {
         var player = BasePlayer.Find(steam_id);
-        if (player == null || !player.IsConnected) return;
+        if (player == null || !player.IsConnected)
+        {
+          return false;
+        }
 
         if (!value)
         {
@@ -915,9 +775,11 @@ namespace Oxide.Plugins
             $"Игрок {steam_id} уведомлён о проверке",
             $"Player {steam_id} was notified about check"
           ));
-          
+
           _RustApp.DrawInterface(player);
         }
+
+        return true;
       }
 
       #endregion
@@ -972,56 +834,56 @@ namespace Oxide.Plugins
 
     private void DrawInterface(BasePlayer player)
     {
-        CuiHelper.DestroyUi(player, CheckLayer);
-        CuiElementContainer container = new CuiElementContainer();
+      CuiHelper.DestroyUi(player, CheckLayer);
+      CuiElementContainer container = new CuiElementContainer();
 
-        container.Add(new CuiButton
-        {
-            RectTransform = { AnchorMin = "0 0.5", AnchorMax = "1 1", OffsetMin = $"-500 -500", OffsetMax = $"500 500" },
-            Button = { Color = HexToRustFormat("#1C1C1C"), Sprite = "assets/content/ui/ui.circlegradient.png" },
-            Text = { Text = "", Align = TextAnchor.MiddleCenter }
-        }, "Under", CheckLayer);
+      container.Add(new CuiButton
+      {
+        RectTransform = { AnchorMin = "0 0.5", AnchorMax = "1 1", OffsetMin = $"-500 -500", OffsetMax = $"500 500" },
+        Button = { Color = HexToRustFormat("#1C1C1C"), Sprite = "assets/content/ui/ui.circlegradient.png" },
+        Text = { Text = "", Align = TextAnchor.MiddleCenter }
+      }, "Under", CheckLayer);
 
-        string text = "<color=#c6bdb4><size=32><b>ВЫ ВЫЗВАНЫ НА ПРОВЕРКУ</b></size></color>\n<color=#958D85>У вас есть <color=#c6bdb4><b>3 минуты</b></color> чтобы отправить дискорд и принять заявку в друзья.\nИспользуйте команду <b><color=#c6bdb4>/contact</color></b> чтобы отправить дискорд.\n\nДля связи с модератором вне дискорда - используйте чат.</color>";
+      string text = "<color=#c6bdb4><size=32><b>ВЫ ВЫЗВАНЫ НА ПРОВЕРКУ</b></size></color>\n<color=#958D85>У вас есть <color=#c6bdb4><b>3 минуты</b></color> чтобы отправить дискорд и принять заявку в друзья.\nИспользуйте команду <b><color=#c6bdb4>/contact</color></b> чтобы отправить дискорд.\n\nДля связи с модератором вне дискорда - используйте чат.</color>";
 
-        container.Add(new CuiLabel
-        {
-            RectTransform = { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMax = "0 0" },
-            Text = { Text = text, Align = TextAnchor.MiddleCenter, Font = "robotocondensed-regular.ttf", FontSize = 16 }
-        }, CheckLayer);
+      container.Add(new CuiLabel
+      {
+        RectTransform = { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMax = "0 0" },
+        Text = { Text = text, Align = TextAnchor.MiddleCenter, Font = "robotocondensed-regular.ttf", FontSize = 16 }
+      }, CheckLayer);
 
-        CuiHelper.AddUi(player, container);
+      CuiHelper.AddUi(player, container);
 
-        Effect effect = new Effect("ASSETS/BUNDLED/PREFABS/FX/INVITE_NOTICE.PREFAB".ToLower(), player, 0, new Vector3(), new Vector3());
-        EffectNetwork.Send(effect, player.Connection);
+      Effect effect = new Effect("ASSETS/BUNDLED/PREFABS/FX/INVITE_NOTICE.PREFAB".ToLower(), player, 0, new Vector3(), new Vector3());
+      EffectNetwork.Send(effect, player.Connection);
     }
 
     private static string HexToRustFormat(string hex)
     {
-        if (string.IsNullOrEmpty(hex))
-        {
-            hex = "#FFFFFFFF";
-        }
+      if (string.IsNullOrEmpty(hex))
+      {
+        hex = "#FFFFFFFF";
+      }
 
-        var str = hex.Trim('#');
+      var str = hex.Trim('#');
 
-        if (str.Length == 6)
-            str += "FF";
+      if (str.Length == 6)
+        str += "FF";
 
-        if (str.Length != 8)
-        {
-            throw new Exception(hex);
-            throw new InvalidOperationException("Cannot convert a wrong format.");
-        }
+      if (str.Length != 8)
+      {
+        throw new Exception(hex);
+        throw new InvalidOperationException("Cannot convert a wrong format.");
+      }
 
-        var r = byte.Parse(str.Substring(0, 2), NumberStyles.HexNumber);
-        var g = byte.Parse(str.Substring(2, 2), NumberStyles.HexNumber);
-        var b = byte.Parse(str.Substring(4, 2), NumberStyles.HexNumber);
-        var a = byte.Parse(str.Substring(6, 2), NumberStyles.HexNumber);
+      var r = byte.Parse(str.Substring(0, 2), NumberStyles.HexNumber);
+      var g = byte.Parse(str.Substring(2, 2), NumberStyles.HexNumber);
+      var b = byte.Parse(str.Substring(4, 2), NumberStyles.HexNumber);
+      var a = byte.Parse(str.Substring(6, 2), NumberStyles.HexNumber);
 
-        Color color = new Color32(r, g, b, a);
+      Color color = new Color32(r, g, b, a);
 
-        return string.Format("{0:F2} {1:F2} {2:F2} {3:F2}", color.r, color.g, color.b, color.a);
+      return string.Format("{0:F2} {1:F2} {2:F2} {3:F2}", color.r, color.g, color.b, color.a);
     }
 
     #endregion
@@ -1094,6 +956,19 @@ namespace Oxide.Plugins
         is_team = channel == ConVar.Chat.ChatChannel.Team,
 
         text = message
+      });
+    }
+
+    #endregion
+
+    #region UX
+
+    [ChatCommand("contact")]
+    private void CmdChatContact(BasePlayer player, string command, string[] args)
+    {
+      _Worker.SendContact(player.UserIDString, String.Join(" ", args), success =>
+      {
+        player.ChatMessage($"status: {success}");
       });
     }
 
