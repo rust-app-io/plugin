d1d786f198bfa650ca6fbf55aa9c2662ad9daf73
Хуган <hougan.main@gmail.com>
2024-04-01 23:17:35 +0200

    Revert "Update RustApp.cs"
    This reverts commit 4c6f6006a076de4d0c3c33d7f8a11f86c91db782.

--- plugins/rust/devblog/236/RustApp.cs
+++ plugins/rust/devblog/236/RustApp.cs
@@ -39,7 +39,7 @@ using Steamworks;
 
 namespace Oxide.Plugins
 {
-    [Info("RustApp", "Hougan & Xacku & Olkuts & Frizen(adaptation)", "1.4.0")]
+    [Info("RustApp", "Hougan & Xacku & Olkuts", "1.4.0")]
     public class RustApp : RustPlugin
     {
         #region Classes 
