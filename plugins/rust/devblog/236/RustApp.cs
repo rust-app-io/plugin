331531fb63077ae5b54dd16b9fd507137477bba3
Хуган <hougan.main@gmail.com>
2024-06-05 19:00:47 +0200

    Поддержка 236

--- plugins/rust/devblog/236/RustApp.cs
+++ plugins/rust/devblog/236/RustApp.cs
@@ -52,7 +52,7 @@ using Apex.DataStructures;
 
 namespace Oxide.Plugins
 {
-    [Info("RustApp", "Hougan & Xacku & Olkuts & Frizen(adaptation)", "1.6.0devblog236")]
+    [Info("RustApp", "Hougan & Xacku & Olkuts & Frizen(adaptation)", "1.7.0devblog236")]
     public class RustApp : RustPlugin
     {
         #region Classes 
@@ -205,7 +205,7 @@ namespace Oxide.Plugins
 
                 if (isError)
                 {
-                    if (body.Contains("cloudflare"))
+                    if (body.Contains("502 bad gateway") || body.Contains("cloudflare"))
                     {
                         onException?.Invoke("rustapp is restarting, wait please");
                     }
@@ -293,7 +293,7 @@ namespace Oxide.Plugins
         {
             private static string Base = "https://ban.rustapp.io";
 
-            public static string Fetch = $"{BanUrls.Base}/plugin";
+            public static string Fetch = $"{BanUrls.Base}/plugin/v2";
         }
 
         class PluginChatMessagesPayload
@@ -1019,6 +1019,9 @@ namespace Oxide.Plugins
                 public long expired_at;
                 public bool ban_ip_active;
                 public bool computed_is_active;
+
+                public int sync_project_id = 0;
+                public bool sync_should_kick = false;
             }
 
             private Dictionary<string, string> PlayersCollection = new Dictionary<string, string>();
@@ -1089,19 +1092,29 @@ namespace Oxide.Plugins
 
                       if (ban != null)
                       {
+                          if (ban.sync_project_id != 0 && !ban.sync_should_kick)
+                          {
+                              return;
+                          }
+
                           if (ban.steam_id == steamId)
                           {
                               var format = ban.expired_at == 0 ? _Settings.ban_reason_format : _Settings.ban_reason_format_temporary;
+                              if (ban.sync_project_id != 0)
+                              {
+                                  format = ban.expired_at == 0 ? _Settings.ban_sync_reason_format : _Settings.ban_sync_reason_format;
+                              }
+
                               var time = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(ban.expired_at + 3 * 60 * 60 * 1000).ToString("dd.MM.yyyy HH:mm");
 
-                              _RustApp.CloseConnection(steamId, format.Replace("%REASON%", ban.reason).Replace("%TIME%", time));
+                              var text = format.Replace("%REASON%", ban.reason).Replace("%TIME%", time);
+                              _RustApp.CloseConnection(steamId, text);
                           }
                           else
                           {
                               _RustApp.CloseConnection(steamId, _Settings.ban_reason_ip_format);
+                              CreateAlertForIpBan(ban, steamId);
                           }
-
-                          CreateAlertForIpBan(ban, steamId);
                       }
                   },
                   () =>
@@ -1479,10 +1492,10 @@ namespace Oxide.Plugins
                     },
                     (err) =>
                     {
-                        _RustApp.Error(
-                  $"Не удалось отправить состояние сервера ({err})",
-                  $"Failed to send server status ({err})"
-                );
+                        //  _RustApp.Error(
+                        //    $"Не удалось отправить состояние сервера ({err})",
+                        //  $"Failed to send server status ({err})"
+                        //);
 
                         /**
                         Возвращаем неудачно отправленные дисконекты
@@ -1541,6 +1554,7 @@ namespace Oxide.Plugins
 
         public class QueueWorker : BaseWorker
         {
+            private List<string> ProcessedQueues = new List<string>();
             public Dictionary<string, bool> Notices = new Dictionary<string, bool>();
 
             public class QueueElement
@@ -1649,11 +1663,18 @@ namespace Oxide.Plugins
 
                     foreach (var queue in queues)
                     {
+                        if (ProcessedQueues.Contains(queue.id))
+                        {
+                            return;
+                        }
+
                         try
                         {
                             var response = QueueProcess(queue);
 
                             responses.Add(queue.id, response);
+
+                            ProcessedQueues.Add(queue.id);
                         }
                         catch (Exception exc)
                         {
@@ -1682,10 +1703,10 @@ namespace Oxide.Plugins
                     (data, raw) => callback(data),
                     (err) =>
                     {
-                        _RustApp.Error(
-                  "Не удалось загрузить задачи из очередей",
-                  "Failed to retreive queue"
-                );
+                        //       _RustApp.Error(
+                        // "Не удалось загрузить задачи из очередей",
+                        //  "Failed to retreive queue"
+                        //);
                     }
                   );
             }
@@ -1694,8 +1715,14 @@ namespace Oxide.Plugins
             {
                 Request<List<QueueElement>>(QueueUrls.Fetch, RequestMethod.PUT, new { data = responses })
                   .Execute(
-                    (data, raw) => { },
-                    (err) => { }
+                    (data, raw) =>
+                    {
+                        ProcessedQueues = new List<string>();
+                    },
+                    (err) =>
+                    {
+                        ProcessedQueues = new List<string>();
+                    }
                   );
             }
 
@@ -2148,6 +2175,13 @@ namespace Oxide.Plugins
             [JsonProperty("[Ban] Message format when kicking due to IP")]
             public string ban_reason_ip_format = "Вам ограничен вход на сервер!";
 
+
+            [JsonProperty("[Ban-Sync] Kick message format (%REASON% - ban reason)")]
+            public string ban_sync_reason_format = "Обнаружена блокировка на другом проекте, причина: %REASON%";
+
+            [JsonProperty("[Ban-Sync] Kick message format temporary (%REASON% - ban reason)")]
+            public string ban_sync_reason_format_temporary = "Обнаружена блокировка на другом проекте до %TIME% МСК, причина: %REASON%";
+
             [JsonProperty("[Custom Actions] Allow custom actions")]
             public bool custom_actions_allow = true;
 
@@ -2170,6 +2204,9 @@ namespace Oxide.Plugins
                     ban_reason_format = "Вы навсегда забанены на этом сервере, причина: %REASON%",
                     ban_reason_format_temporary = "Вы забанены на этом сервере до %TIME% МСК, причина: %REASON%",
                     ban_reason_ip_format = "Вам ограничен вход на сервер!",
+
+                    ban_sync_reason_format = "Обнаружена блокировка на другом проекте, причина: %REASON%",
+                    ban_sync_reason_format_temporary = "Обнаружена блокировка на другом проекте до %TIME% МСК, причина: %REASON%",
 
                     custom_actions_allow = true
                 };
@@ -3412,7 +3449,7 @@ namespace Oxide.Plugins
             NextTick(() => { _Worker.Action.SendSignage(new SignageUpdate(player, signage)); });
         }
 
-  
+
 
         private object CanUpdateSign(BasePlayer player, BaseEntity entity)
         {
@@ -3428,7 +3465,7 @@ namespace Oxide.Plugins
             return null;
         }
 
-      
+
         #endregion
 
         private void OnEntityBuilt(Planner plan, GameObject go)
